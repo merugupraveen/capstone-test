7018a762f715fdbcfbd27d8cce8ebb2b08eb5ec4
Praveen Merugu <praveen_merugu@epam.com>
2026-04-13 01:06:17 +0530

    tets

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const env = {
   uiBaseUrl: process.env.UI_BASE_URL ?? "http://localhost:8080/",
   apiBaseUrl:
-    process.env.API_BASE_URL ?? "https://jsonplaceholder.typicode.com",
+    process.env.API_BASE_URL ?? "http://localhost:8080",
   apiAuthToken: process.env.API_AUTH_TOKEN,
   defaultTimeoutMs: Number(process.env.DEFAULT_TIMEOUT_MS ?? 30000)
 };
